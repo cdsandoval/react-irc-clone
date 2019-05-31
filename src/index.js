74a422f5add97a26d74ae6c34bf750706a606070
Carlos Sandoval <iznomag@gmail.com>
2019-05-31 14:35:23 -0500

    Setting component

--- src/index.js
+++ src/index.js
@@ -3,35 +3,9 @@
 import React from "react";
 import { render } from "react-dom";
 import { jsx } from "@emotion/core";
-import ListMessages from "./components/listMessages.jsx";
+import ListMessages from "./components/listMessages/listMessages";
 
 function Codeable() {
-  let message = [
-    {
-      key: 1,
-      Author: "Carlos",
-      date: "01/01/1111",
-      text: "Hola"
-    },
-    {
-      key: 2,
-      Author: "Daniel",
-      date: "01/01/1111",
-      text: "Hola"
-    },
-    {
-      key: 3,
-      Author: "Carlos",
-      date: "01/01/1111",
-      text: "Como estas"
-    },
-    {
-      key: 4,
-      Author: "Daniel",
-      date: "01/01/1111",
-      text: "Bien"
-    }
-  ];
   return (
     <div>
       <ListMessages message={message} />
