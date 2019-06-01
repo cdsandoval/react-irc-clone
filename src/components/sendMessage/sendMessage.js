2fc553b3929bf863cb1c398ab1db9b8218adab63
Lian Nivin <liamrn94@gmail.com>
2019-06-01 13:05:43 -0500

    Delete unnecesary id's

--- src/components/sendMessage/sendMessage.js
+++ src/components/sendMessage/sendMessage.js
@@ -8,16 +8,13 @@ function SendMessage() {
     <form css={sendMessageCss} className="send-message">
       <input
         type="text"
-        id="js-input-user-message"
         aria-label="Chat here"
         className="input-text"
         placeholder="Chat here..."
         autocomplete="off"
         autofocus
       />
-      <button id="js-add-user-message" className="button -dark submit">
-        Send
-      </button>
+      <button className="button -dark submit">Send</button>
     </form>
   );
 }
