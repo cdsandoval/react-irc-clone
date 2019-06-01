60b6e1259b6c2ec62cc6b0d66a2df2473c0efb1d
Ricardo <ry.yrupailla@gmail.com>
2019-06-01 12:34:37 -0500

    Clean css

--- src/home.js
+++ src/home.js
@@ -2,6 +2,15 @@ import React from "react";
 import { Global, css } from "@emotion/core";
 import Tabs from "./components/tab/tabs";
 import base from "./assets/css/base";
+import layout from "./assets/css/layout";
+
+import button from "./assets/css/components/buttons";
+import inputs from "./assets/css/components/inputs";
+import window from "./assets/css/components/window";
+import lightBox from "./assets/css/components/light-box";
+import message from "./assets/css/components/message";
+import separator from "./assets/css/components/separator";
+import home from "./assets/css/pages/home";
 
 function Home({ url }) {
   const ws = React.useRef(null);
@@ -49,6 +58,16 @@ function Home({ url }) {
   return (
     <>
       <Global styles={base} />
+      <Global styles={layout} />
+
+      <Global styles={button} />
+      <Global styles={inputs} />
+      <Global styles={window} />
+      <Global styles={lightBox} />
+      <Global styles={message} />
+      <Global styles={separator} />
+
+      <Global styles={home} />
 
       <ul>
         {listMessage.map(message => (
